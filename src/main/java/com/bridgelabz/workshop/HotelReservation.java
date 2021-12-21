665a1fd412a368e31f90164758da8578c3a15e85
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-21 08:18:30 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/workshop/HotelReservation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.workshop;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Hotel Reservation System");
+
+	}
+
+}
